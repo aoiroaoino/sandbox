3d5e28b3eebbe860b9fd9a986112248cad3ca6a9
aoiroaoino <aoiro.aoino@gmail.com>
2020-03-21 22:53:35 +0900

    [lang-monkey] lexer impl

---
+++ lang-monkey/go.mod
@@ -0,0 +1,3 @@
+module github.com/aoiroaoino/sandbox/lang-monkey
+
+go 1.12
---
+++ lang-monkey/src/monkey/repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/aoiroaoino/sandbox/lang-monkey/src/monkey/lexer"
+	"github.com/aoiroaoino/sandbox/lang-monkey/src/monkey/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
